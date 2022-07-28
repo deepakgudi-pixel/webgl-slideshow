9c10c0bb490254553415acab230974507df0cdea
deepakgudi-pixel <japanesen52018@gmail.com>
2022-07-28 20:19:46 +0530

    created canvas

--- frag.js
+++ frag.js
@@ -1,9 +1,87 @@
 const frag = `
 
+#ifdef GL_ES
+precision highp float;
+#endif
+
+#define MAX 6
+
+uniform float u_time;
+uniform vec2 U_resolution;
+
+uniform float timeline;
+
+uniform sampler2D image1;
+uniform sampler2D image2;
+uniform sampler2D image3;
+uniform sampler2D image4;
+uniform sampler2D textures[MAX];
+
+uniform int startIndex;
+uniform int endIndex;
+
+varying vec3 v_normal;
+varying vec2 v_texcoord;
+
+
 ${includes}
 
-void main () {
-    gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
-}
+vec4 sampleColor(int index, vec2 uv){
+    if (index == 0){
+       return texture2D(image1, uv);
+    }else if (index == 1){
+       return texture2D(image2, uv);
+    }else if (index == 2){
+        return texture2D(image3, uv); 
+    }else if (index == 3){
+        return texture2D(image4, uv);
+    }
+     return vec4(1.0, 1.0, 1.0, 1.0);
+   }
+   
+   
+   void main(void)
+   {
+   
+       vec2 uv = v_texcoord;
+       uv -= 0.5;
+       
+       float amplitude = 3.5;
+       
+       float wave = fbm(amplitude * uv + 0.2 * u_time);
+       
+       //creating fade in and fade out using timelines
+       float strength = smoothstep(0.0, 2.0, timeline) - smoothstep(2.0, 3.0, timeline);
+       
+       float distortion = mix(1., 1.0 + strength, wave);
+       
+       uv *= distortion;
+       uv += 0.5;
+       
+       //flip the texture
+       uv.y = 1.0 - uv.y;
+       
+       //get rid of extras
+       if(uv.x < 0.0 || uv.x > 1.0 || uv.y < 0.0 || uv.y > 1.0 ){
+         discard;
+       }
+       
+       //pick images
+       vec4 startTexture = sampleColor(startIndex, uv);
+       vec4 endTexture = sampleColor(endIndex, uv);
+       
+       
+       //tween
+       float changeTimeline = smoothstep(0.5, 2., timeline);
+       float mixer = 1.0 - step(changeTimeline, wave);
+       
+       vec4 color = mix(startTexture, endTexture, mixer);
+       
+       gl_FragColor = color;
+   }
+   
+
+
+
 
 `
--- include.js
+++ include.js
@@ -1,2 +1,44 @@
 const includes = `
+
+//fbm natural noise 
+#define NUM_OCTAVES 6
+precision highp float;
+
+float rand(vec2 n) { 
+	return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+}
+
+vec2 rotate(vec2 v, float a) {
+	float s = sin(a);
+	float c = cos(a);
+	mat2 m = mat2(c, -s, s, c);
+	return m * v;
+}
+
+float noise(vec2 p){
+	vec2 ip = floor(p);
+	vec2 u = fract(p);
+	u = u*u*(3.0-2.0*u);
+	
+	float res = mix(
+	mix(rand(ip),rand(ip+vec2(1.0,0.0)),u.x),
+	mix(rand(ip+vec2(0.0,1.0)),rand(ip+vec2(1.0,1.0)),u.x),u.y);
+	return res*res;
+}
+
+float fbm(in vec2 _st) {
+    float v = 0.0;
+    float a = 0.5;
+    vec2 shift = vec2(100.0);
+    // Rotate to reduce axial bias
+    mat2 rot = mat2(cos(0.5), sin(0.5),
+                    -sin(0.5), cos(0.50));
+    for (int i = 0; i < NUM_OCTAVES; ++i) {
+        v += a * noise(_st);
+        _st = rot * _st * 2.0 + shift;
+        a *= 0.5;
+    }
+    return v;
+}
+
 `
